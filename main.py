4142a8a696cc225c344280c61dc51b0f9c479f78
rydercatonio <rcatonio@ualberta.ca>
2023-01-12 17:16:04 -0700

    Switching to desktop

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
+# # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 
 def main():
+    email_addresses = ["rydercatonio8@gmail.com","catonio@ualberta.ca"]
+
+
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
@@ -37,15 +40,65 @@ def main():
     try:
         # Call the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
 
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+        for person in email_addresses:
+            test = "from:" + person + " " + "is:unread"
+            messages = service.users().messages().list(userId="me",labelIds=['INBOX'],q=test).execute()
+            for message in messages['messages']:
+                msg = service.users().messages().get(userId="me",id=message['id']).execute()
+                print(msg['snippet'])
+
+
+
+
+
+
+
+
+
+
+
+        # print(messages)
+
+        # for message in messages:
+        #     print(message)
+        # #     # print(message['id'])
+        # #     # msg = service.users().messages().get(userId="me",id=message['id']).execute()
+        # #     # email_body = msg['payload']
+        # #     # print(email_body)
+
+
+
+        # print(messages)
+        # msg = messages['messages']
+
+        # for m in msg:
+        #     print(m['snippet'])
+
+
+
+
+        # print(msg)
+
+
+
+
+
+
+        # message = service.users().messages().get(userId="me",id="185a396e381b07ad",format="raw").execute()
+
+
+        # print(messages['snippet'])
+        # print(results)
+
+
+
+        # results = service.users().messages().list(userId="me",labelIds=['INBOX'],q="is:unread").execute()
+        # messages= results.get('messages',[])
+        # print(messages)
+        # # results = service.users().labels().list(userId='me').execute()
+        # # labels = results.get('labels', [])
+
 
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
